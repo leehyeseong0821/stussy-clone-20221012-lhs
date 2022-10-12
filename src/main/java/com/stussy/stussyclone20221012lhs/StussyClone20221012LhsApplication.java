c07c629a463a9996ac6aa36cd3ceb83d1f12d190
leehyeseong0821 <lhs5567@naver.com>
2022-10-12 23:52:57 +0900

    2022-10-12

---
+++ src/main/java/com/stussy/stussyclone20221012lhs/StussyClone20221012LhsApplication.java
@@ -0,0 +1,13 @@
+package com.stussy.stussyclone20221012lhs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StussyClone20221012LhsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StussyClone20221012LhsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stussy/stussyclone20221012lhs/StussyClone20221012LhsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stussy.stussyclone20221012lhs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StussyClone20221012LhsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
